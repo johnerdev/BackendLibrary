b1620651f3b1f2708b9b90049176d71d94e657d1
Johson Rodriguez <sukerjc@gmail.com>
2024-12-25 16:12:48 -0500

    With faih

---
+++ src/main/java/com/bilioteca/lordbyron/LordbyronApplication.java
@@ -0,0 +1,13 @@
+package com.bilioteca.lordbyron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordbyronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LordbyronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bilioteca/lordbyron/LordbyronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilioteca.lordbyron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LordbyronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
